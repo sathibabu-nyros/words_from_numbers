59be24db397c3bb39e6ce592cd14dd8853a59758
sathibabu-nyros <sathibabu_nyros@yahoo.com>
2016-01-19 14:10:19 +0530

    first commit

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # WordsFromNumbers
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/words_from_numbers`. To experiment with that code, run `bin/console` for an interactive prompt.
 
-TODO: Delete this and the text above, and describe your gem
 
 ## Installation
 
@@ -24,7 +22,10 @@ Or install it yourself as:
 
 TODO: Write usage instructions here
 
-## Development
+words_from_numbers(44280)  => FOURTY FOUR THOUSAND TWO HUNDRED AND EIGHTY ONLY
+
+words_from_numbers(37015.20)  => THIRTY SEVEN THOUSAND  FIFTEEN AND CENTS TWENTY ONLY
+
 
 After checking out the repo, run `bin/setup` to install dependencies. Then, run `bin/console` for an interactive prompt that will allow you to experiment.
 
--- lib/words_from_numbers.rb
+++ lib/words_from_numbers.rb
@@ -1,5 +1,51 @@
 require "words_from_numbers/version"
 
 module WordsFromNumbers
-  # Your code goes here...
+
+	def self.subhundred number
+	  ones = %w{zero one two three four five six seven eight nine
+	            ten eleven twelve thirteen fourteen fifteen
+	            sixteen seventeen eighteen nineteen}
+	  tens = %w{zero ten twenty thirty fourty fifty sixty seventy eighty ninety}
+	  
+	  subhundred = number % 100
+	  return [ones[subhundred]] if subhundred < 20
+	  return [tens[subhundred / 10], ones[subhundred % 10]]
+	end
+
+	def self.subthousand number	 
+	
+	  hundreds = (number % 1000) / 100
+	  tens = number % 100
+	  s = []	 
+	  s = s + subhundred(hundreds) + ["hundred"] unless hundreds == 0
+	  s = s + ["and"] unless hundreds == 0 or tens == 0
+	  s = s + [subhundred(tens)] unless tens == 0
+	  puts "#{s}"
+	  s
+	end
+
+	def self.decimals number
+	  return [] unless number.to_s['.']
+	  number = number.to_s.split('.')[1]  
+	 
+	  digits =  subhundred number.to_i if number.to_i > 0
+	  #digits.present? ? ["and"] + digits + ['cents'] : []
+	   digits.present? ? [" and"] +  ['cents']  + digits : []  
+	end
+
+	def self.words_from_numbers number
+	  steps = [""] + %w{thousand million billion trillion quadrillion quintillion sextillion}
+	  result = []
+	  n = number.to_i
+	  steps.each do |step|
+	    x = n % 1000
+	    unit = (step == "") ? [] : [step]	   
+	    result = subthousand(x) + unit  + result unless x == 0   
+	    n = n / 1000
+	  end
+	  result = ["zero"] if result.empty?	
+	  result.join(' ').gsub('zero','').strip  	  
+	end
+
 end
